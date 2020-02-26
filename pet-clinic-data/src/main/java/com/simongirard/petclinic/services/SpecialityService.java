4c445aa7ebd4ea28b7f529cef61cd15d381128d6
SimonGirardSfeir <girard.s@sfeir.com>
2020-02-26 16:07:23 +0100

    speciality map service addes. Closes #30

---
+++ pet-clinic-data/src/main/java/com/simongirard/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.simongirard.petclinic.services;
+
+import com.simongirard.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/simongirard/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.simongirard.petclinic.services.map;
+
+import com.simongirard.petclinic.model.Speciality;
+import com.simongirard.petclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
