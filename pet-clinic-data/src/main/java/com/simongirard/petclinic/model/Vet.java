e06e04e5f7b0135de33a8c1d5f94e408d7d5d920
SimonGirardSfeir <girard.s@sfeir.com>
2020-02-28 18:01:33 +0100

    Vet JPA entity Closes #36.

--- pet-clinic-data/src/main/java/com/simongirard/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/simongirard/petclinic/model/Vet.java
@@ -1,10 +1,20 @@
 package com.simongirard.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_speciality", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
