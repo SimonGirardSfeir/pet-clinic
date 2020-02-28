7648fde7c881ed281fd2bc4aaba87cff73252e96
SimonGirardSfeir <girard.s@sfeir.com>
2020-02-28 19:30:57 +0100

    JPA service for Vet. Closes #40.

---
+++ pet-clinic-data/src/main/java/com/simongirard/petclinic/services/springdatajpa/VetJpaService.java
@@ -0,0 +1,55 @@
+package com.simongirard.petclinic.services.springdatajpa;
+
+import com.simongirard.petclinic.model.Vet;
+import com.simongirard.petclinic.repositories.VetRepository;
+import com.simongirard.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return vetRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
