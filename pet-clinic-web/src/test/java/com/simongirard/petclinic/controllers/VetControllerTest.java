ff263a1f67b8f3bde20030543fddb3ea53de496e
SimonGirardSfeir <girard.s@sfeir.com>
2020-03-05 19:00:44 +0100

    Spring MVC tests for controllers. Closes #51.

---
+++ pet-clinic-web/src/test/java/com/simongirard/petclinic/controllers/VetControllerTest.java
@@ -0,0 +1,57 @@
+package com.simongirard.petclinic.controllers;
+
+import com.simongirard.petclinic.model.Vet;
+import com.simongirard.petclinic.services.VetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class VetControllerTest {
+
+    @Mock
+    private VetService vetService;
+
+    @InjectMocks
+    private VetController vetController;
+
+    private Set<Vet> vets;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        vets = new HashSet<>();
+        vets.add(Vet.builder().id(1L).build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vetController).build();
+    }
+
+    @Test
+    void listVets() throws Exception {
+        when(vetService.findAll()).thenReturn(vets);
+
+        mockMvc.perform(get("/vets"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("vets/index"))
+                .andExpect(model().attribute("vets", hasSize(1)));
+
+        verify(vetService).findAll();
+    }
+}
