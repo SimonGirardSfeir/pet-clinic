4084712fd1738f0bf294971fd1c449d5fe750e20
SimonGirardSfeir <girard.s@sfeir.com>
2020-04-21 18:06:23 +0200

    JSON endpoint for Vet List. Closes #65.

--- pet-clinic-web/src/main/java/com/simongirard/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/simongirard/petclinic/controllers/VetController.java
@@ -1,10 +1,14 @@
 package com.simongirard.petclinic.controllers;
 
+import com.simongirard.petclinic.model.Vet;
 import com.simongirard.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping
 @Controller
@@ -16,10 +20,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @GetMapping({"/vets", "/vets/index", "vets/index.html", "/vets.html"})
+    @GetMapping("vets/index.html")
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+       return vetService.findAll();
     }
 }
